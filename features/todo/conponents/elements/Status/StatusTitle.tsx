1e7adc5da7bd2109a1c48494c9feabe602423276
taichisakai <sakai111893@gmail.com>
2025-01-05 23:40:07 +0900

    focus付与とusecallback,memo化

--- features/todo/conponents/elements/Status/StatusTitle.tsx
+++ features/todo/conponents/elements/Status/StatusTitle.tsx
@@ -4,7 +4,7 @@ import { Box, IconButton } from '@mui/material';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import SelectListModal from '@/features/todo/conponents/elements/Modal/SelectListModal';
 import DeleteModal from '@/features/todo/conponents/elements/Modal/DeleteModal';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useCallback, memo } from 'react';
 
 type Prop = {
   title: string;
@@ -17,166 +17,167 @@ type Prop = {
   setInput: (input: { status: string }) => void;
 };
 
-const StatusTitle = ({
-  title,
-  status,
-  id,
-  isEditing,
-  editList,
-  deleteList,
-  setListEdit,
-  setInput,
-}: Prop) => {
-  const [selectModalIsOpen, setSelectModalIsOpen] = useState({
-    order: false,
-    list: false,
-    rename: false,
-  });
-  const [deleteIsModalOpen, setDeleteIsModalOpen] = useState(false);
-  const [textRename, setTextRename] = useState(false);
-  const [inputValue, setInputValue] = useState(status || title); // 初期値を管理
+const StatusTitle = memo(
+  ({
+    title,
+    status,
+    id,
+    isEditing,
+    editList,
+    deleteList,
+    setListEdit,
+    setInput,
+  }: Prop) => {
+    const [selectModalIsOpen, setSelectModalIsOpen] = useState({
+      order: false,
+      list: false,
+      rename: false,
+    });
+    const [deleteIsModalOpen, setDeleteIsModalOpen] = useState(false);
+    const [textRename, setTextRename] = useState(false);
+    const [inputValue, setInputValue] = useState(status || title); // 初期値を管理
 
-  const modalRef = useRef<HTMLDivElement>(null);
-  const deleteModalRef = useRef<HTMLDivElement>(null);
-  const textFieldRef = useRef<HTMLDivElement>(null);
-  // const inputRef = useRef<HTMLInputElement>(null);
+    const modalRef = useRef<HTMLDivElement>(null);
+    const deleteModalRef = useRef<HTMLDivElement>(null);
+    const textFieldRef = useRef<HTMLDivElement>(null);
 
-  // 外部からの status 変更を監視して同期
-  useEffect(() => {
-    setInputValue(status || title);
-  }, [status, title]);
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      const target = event.target as Node;
-      // `textFieldRef` が参照する要素の子要素から `input` を取得
-      // const inputElement = textFieldRef.current?.querySelector('input');
-
-      // if (inputElement) {
-      //   console.log('Input element ID:', inputElement.value); // `value` を表示
-      // }
-
-      // クリックイベントがセレクトモーダル外で発生した場合
-      if (
-        modalRef.current &&
-        !modalRef.current.contains(target) &&
-        !deleteModalRef.current
-      ) {
-        console.log('Click detected outside, updating state...');
-        setSelectModalIsOpen((prevState) => ({
-          ...prevState,
-          order: false,
-          list: false,
-          rename: false,
-        }));
+    // 初回レンダリング時にフォーカスを当てる
+    useEffect(() => {
+      const inputElement = textFieldRef.current?.querySelector('input');
+      if (isEditing && inputElement) {
+        inputElement.focus();
       }
+    }, [isEditing]); // `isEditing`がtrueの時に実行
 
-      if (textFieldRef.current?.contains(target) === true) {
-        setTextRename(true);
-        console.log('setTextRename called with true');
-      } else if (textFieldRef.current?.contains(target) === false) {
-        setTextRename(false);
-        setInput({ status: '' });
-        // isEditing = false;
-        console.log('setTextRename called with false');
-      }
-    };
+    // 外部からの status 変更を監視して同期
+    useEffect(() => {
+      setInputValue(status || title);
+    }, [status, title]);
 
-    // 画面をクリックした際に handleClickOutside 関数を実行
-    document.addEventListener('mousedown', handleClickOutside);
-    // コンポーネントがアンマウントされた時にhandleClickOutside 関数を実行。クリーンアップ関数。
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [modalRef, deleteModalRef, textFieldRef, setInput]);
+    // SelectListModalの挙動制御
+    // useCallbackを使用
+    const handleClickOutside = useCallback(
+      (event: MouseEvent) => {
+        const target = event.target as Node;
 
-  console.log(isEditing);
-
-  return (
-    <Box
-      component="div"
-      id={id}
-      sx={{
-        textAlign: 'center',
-        position: 'relative',
-      }}
-    >
-      {textRename && isEditing ? (
-        <div ref={textFieldRef}>
-          <input
-            id={`${id}_input`}
-            value={inputValue}
-            // ref={inputRef}
-            onChange={(e) => {
-              setInputValue(e.target.value); // ローカル状態を更新
-              editList(id, e.target.value, title);
-            }}
-            style={{ textAlign: 'center' }}
-          />
-        </div>
-      ) : (
-        title
-      )}
-      <IconButton
-        onClick={() =>
+        // クリックイベントがセレクトモーダル外で発生した場合
+        if (
+          modalRef.current &&
+          !modalRef.current.contains(target) &&
+          !deleteModalRef.current
+        ) {
+          console.log('Click detected outside, updating state...');
           setSelectModalIsOpen({
-            ...selectModalIsOpen,
-            order: true,
-            list: true,
-            rename: true,
-          })
+            order: false,
+            list: false,
+            rename: false,
+          });
         }
+
+        if (textFieldRef.current?.contains(target)) {
+          setTextRename(true);
+          console.log('setTextRename called with true');
+        } else {
+          setTextRename(false);
+          setInput({ status: '' });
+          console.log('setTextRename called with false');
+        }
+      },
+      [setInput],
+    );
+
+    useEffect(() => {
+      // 画面をクリックした際に handleClickOutside 関数を実行
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => {
+        // コンポーネントがアンマウントされた時にhandleClickOutside 関数を実行。クリーンアップ関数。
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }, [handleClickOutside]);
+
+    console.log(isEditing);
+
+    return (
+      <Box
+        component="div"
+        id={id}
         sx={{
-          position: 'absolute',
-          top: 0,
-          right: 10,
-          p: 0,
+          textAlign: 'center',
+          position: 'relative',
         }}
       >
-        <MoreVertIcon />
-      </IconButton>
-      {(selectModalIsOpen.order ||
-        selectModalIsOpen.list ||
-        selectModalIsOpen.rename) && (
-        <div ref={modalRef}>
-          <SelectListModal
-            id={id}
-            selectModalIsOpen={selectModalIsOpen}
-            setListEdit={setListEdit}
-            setSelectModalIsOpen={setSelectModalIsOpen}
-            setDeleteIsModalOpen={setDeleteIsModalOpen}
-            setTextRename={(value) => {
-              setTextRename(value);
-              if (value) {
-                console.log(value);
-                // inputRef.current?.focus();
-              }
-            }}
-          />
-        </div>
-      )}
+        {textRename && isEditing ? (
+          <div ref={textFieldRef}>
+            <input
+              id={`${id}_input`}
+              value={inputValue}
+              onChange={(e) => {
+                setInputValue(e.target.value); // ローカル状態を更新
+                editList(id, e.target.value, title);
+              }}
+              style={{ textAlign: 'center' }}
+            />
+          </div>
+        ) : (
+          title
+        )}
+        <IconButton
+          onClick={() =>
+            setSelectModalIsOpen({
+              ...selectModalIsOpen,
+              order: true,
+              list: true,
+              rename: true,
+            })
+          }
+          sx={{
+            position: 'absolute',
+            top: 0,
+            right: 10,
+            p: 0,
+          }}
+        >
+          <MoreVertIcon />
+        </IconButton>
+        {(selectModalIsOpen.order ||
+          selectModalIsOpen.list ||
+          selectModalIsOpen.rename) && (
+          <div ref={modalRef}>
+            <SelectListModal
+              id={id}
+              selectModalIsOpen={selectModalIsOpen}
+              setListEdit={setListEdit}
+              setSelectModalIsOpen={setSelectModalIsOpen}
+              setDeleteIsModalOpen={setDeleteIsModalOpen}
+              setTextRename={setTextRename}
+            />
+          </div>
+        )}
 
-      {deleteIsModalOpen && id && (
-        <div ref={deleteModalRef}>
-          <DeleteModal
-            title={title}
-            onDelete={() => {
-              console.log('onDelete triggered'); // コンソールログを追加
-              if (id) {
-                deleteList(id, title);
-                setSelectModalIsOpen({ ...selectModalIsOpen, list: false });
+        {deleteIsModalOpen && id && (
+          <div ref={deleteModalRef}>
+            <DeleteModal
+              title={title}
+              onDelete={() => {
+                console.log('onDelete triggered'); // コンソールログを追加
+                if (id) {
+                  deleteList(id, title);
+                  setSelectModalIsOpen({ ...selectModalIsOpen, list: false });
+                }
+              }}
+              modalIsOpen={deleteIsModalOpen}
+              setModalIsOpen={setDeleteIsModalOpen}
+              setSelectModalIsOpen={(listModal) =>
+                setSelectModalIsOpen({ ...selectModalIsOpen, list: listModal })
               }
-            }}
-            modalIsOpen={deleteIsModalOpen}
-            setModalIsOpen={setDeleteIsModalOpen}
-            setSelectModalIsOpen={(listModal) =>
-              setSelectModalIsOpen({ ...selectModalIsOpen, list: listModal })
-            }
-          />
-        </div>
-      )}
-    </Box>
-  );
-};
+            />
+          </div>
+        )}
+      </Box>
+    );
+  },
+);
+
+StatusTitle.displayName = 'StatusTitle';
 
 export default StatusTitle;
