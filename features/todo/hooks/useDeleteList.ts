838f845fcdea18e53c825c90618e9af33d5826e8
taichisakai <sakai111893@gmail.com>
2025-01-13 21:49:14 +0900

    useDeleteLIstsのsetListsとsetTodosを修正

--- features/todo/hooks/useDeleteList.ts
+++ features/todo/hooks/useDeleteList.ts
@@ -9,14 +9,12 @@ import { apiRequest } from '@/app/libs/apis';
 
 type DeleteListProps = {
   todos: TodoListProps[];
-  lists: StatusListProps[];
   setTodos: React.Dispatch<React.SetStateAction<TodoListProps[]>>;
   setLists: React.Dispatch<React.SetStateAction<StatusListProps[]>>;
 };
 
 export const useDeleteList = ({
   todos,
-  lists,
   setTodos,
   setLists,
 }: DeleteListProps) => {
@@ -32,7 +30,7 @@ export const useDeleteList = ({
         );
         console.log(result);
         // client
-        setLists(lists.filter((list) => list.id !== id)); // todo.id が id と一致しない list だけを残す新しい配列を作成
+        setLists((prevLists) => prevLists.filter((list) => list.id !== id)); // todo.id が id と一致しない list だけを残す新しい配列を作成
 
         // server side
         // 該当するtodosを削除
@@ -53,13 +51,15 @@ export const useDeleteList = ({
           console.log(todoResult);
 
           // client
-          setTodos(todos.filter((todo) => todo.status !== title)); // todo.id が id と一致しない todo だけを残す新しい配列を作成
+          setTodos((prevTodos) =>
+            prevTodos.filter((todo) => todo.status !== title),
+          ); // todo.id が id と一致しない todo だけを残す新しい配列を作成
         }
       } catch (error) {
         console.error('Failed to delete list and related todos:', error);
       }
     },
-    [todos, lists, setTodos, setLists], // 第二引数に依存配列を指定
+    [todos, setTodos, setLists], // 第二引数に依存配列を指定
   );
   return {
     deleteList,
