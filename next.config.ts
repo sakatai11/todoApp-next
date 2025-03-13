966179dd34859649aef49d7d081007bf9e083d95
Sakai Taichi <130420550+sakatai11@users.noreply.github.com.>
2025-03-13 23:00:52 +0900

    キャッシュの設定

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    staleTimes: {
+      dynamic: 0,
+    },
+  },
 };
 
 export default nextConfig;
