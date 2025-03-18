db8311625722757ca22a435a78e838cf7e709402
Sakai Taichi <130420550+sakatai11@users.noreply.github.com.>
2025-03-18 22:58:57 +0900

    Firebase Admin の初期化（既存のインスタンスがない場合のみ実行）

--- app/libs/firebaseAdmin.ts
+++ app/libs/firebaseAdmin.ts
@@ -1,4 +1,9 @@
-import { initializeApp, cert, ServiceAccount } from 'firebase-admin/app';
+import {
+  initializeApp,
+  cert,
+  getApps,
+  ServiceAccount,
+} from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
 // サービスアカウントの設定
@@ -8,10 +13,11 @@ const serviceAccount: ServiceAccount = {
   clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
 };
 
-// Firebase Admin の初期化
-const firebaseAdminApp = initializeApp({
-  credential: cert(serviceAccount),
-});
+// Firebase Admin の初期化（既存のインスタンスがない場合のみ実行）
+const firebaseAdminApp =
+  getApps().length > 0
+    ? getApps()[0]
+    : initializeApp({ credential: cert(serviceAccount) });
 
 export default firebaseAdminApp;
 export const adminDB = getFirestore();
