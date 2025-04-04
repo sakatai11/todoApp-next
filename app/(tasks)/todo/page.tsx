8a727df009b68e752de3aa8df459de8cd426e167
Sakai Taichi <130420550+sakatai11@users.noreply.github.com.>
2025-04-05 00:38:09 +0900

    もとに戻す

--- app/(tasks)/todo/page.tsx
+++ app/(tasks)/todo/page.tsx
@@ -1,12 +1,15 @@
 import * as Todo from '@/features/todo/templates/Index';
-import { fetchFirestoreData } from '@/app/(tasks)/actions/fetchFirestoreData';
 
 export default async function TodoPage() {
-  // const response = await fetch(`${process.env.NEXTAUTH_URL}/api/info`, {
-  //   cache: 'no-store',
-  // });
+  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/info`, {
+    cache: 'no-store',
+  });
 
-  const { todos, lists } = await fetchFirestoreData();
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  const { todos, lists } = await response.json();
 
   return (
     <>
