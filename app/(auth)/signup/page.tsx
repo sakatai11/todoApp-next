4a04ace547c6488452fd11570ac073f042262bb1
Sakai Taichi <130420550+sakatai11@users.noreply.github.com>
2025-05-06 22:29:03 +0900

    本番環境ではサインアップページを表示しない

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,7 +1,11 @@
+import { notFound } from 'next/navigation';
 import * as Sign from '@/features/sign/templates/index';
 import Template from '@/app/template';
 
 export default function SignUpPage() {
+  if (process.env.NODE_ENV === 'production') {
+    notFound();
+  }
   return (
     <Template showHeader={false}>
       <Sign.ContactWrapper />
