04f5cf41cc2fce9251ef63dbcd2d93f093150088
Sakai Taichi <130420550+sakatai11@users.noreply.github.com>
2025-06-03 13:54:46 +0900

    firebaseの型に合わせて修正

--- data/todos.ts
+++ data/todos.ts
@@ -1,35 +1,36 @@
 // mocks/data/todos.ts
 import { TodoListProps } from '@/types/todos';
+import { Timestamp } from 'firebase-admin/firestore';
 
 const mockTodos: TodoListProps[] = [
   {
     id: 'todo-1',
-    updateTime: 1746271892078,
-    createdTime: 1746271892078,
+    updateTime: Timestamp.fromDate(new Date(1746271892078)),
+    createdTime: Timestamp.fromDate(new Date(1746271892078)),
     text: 'Next.js App Routerの学習',
     status: 'in-progress',
     bool: true,
   },
   {
     id: 'todo-2',
-    updateTime: 1744749991599,
-    createdTime: 1744749991599,
+    updateTime: Timestamp.fromDate(new Date(1744749991599)),
+    createdTime: Timestamp.fromDate(new Date(1744749991599)),
     text: 'Nuxt3の学習',
     status: 'in-progress',
     bool: false,
   },
   {
     id: 'todo-3',
-    updateTime: 1746100000000,
-    createdTime: 1746100000000,
+    updateTime: Timestamp.fromDate(new Date(1746100000000)),
+    createdTime: Timestamp.fromDate(new Date(1746100000000)),
     text: 'MSWの実装',
     status: 'todo',
     bool: false,
   },
   {
     id: 'todo-4',
-    updateTime: 1746050000000,
-    createdTime: 1746050000000,
+    updateTime: Timestamp.fromDate(new Date(1746050000000)),
+    createdTime: Timestamp.fromDate(new Date(1746050000000)),
     text: 'TypeScript最適化',
     status: 'done',
     bool: true,
